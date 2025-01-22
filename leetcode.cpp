2cc06a31d144c6990a3397488c974c7db21c8c72
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2025-01-22 16:16:03 +0500

    Two sum problem in C++

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,21 +1,20 @@
 class Solution
 {
 public:
-    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+    vector<int> twoSum(vector<int> &nums, int target)
     {
-        vector<int> mergedArr = nums1;
-        mergedArr.insert(mergedArr.end(), nums2.begin(), nums2.end());
-
-        sort(mergedArr.begin(), mergedArr.end());
-
-        int n = mergedArr.size();
-        if (n % 2 != 0)
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
         {
-            return mergedArr[n / 2];
+            for (int j = i + 1; j < n; j++)
+            {
+
+                if (nums[j] + nums[i] == target)
+                {
+                    return {i, j};
+                }
+            }
         }
-        else
-        {
-            return (mergedArr[n / 2 - 1] + mergedArr[n / 2]) / 2.0;
-        }
+        return {};
     }
 };
