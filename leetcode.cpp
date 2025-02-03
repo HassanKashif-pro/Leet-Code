b155a01827584e2b6e64ded2304f32381770d007
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2025-02-03 15:40:54 +0500

    Container With Most Water in Leet code

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,58 +1,26 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 class Solution
 {
 public:
-    bool isMatch(string s, string p)
+    int maxArea(vector<int> &height)
     {
-        int m = s.size(), n = p.size();
-        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        int left = 0;
+        int right = height.size() - 1;
+        int maxArea = 0;
 
-        dp[0][0] = true; // Both empty strings match
-
-        // Handling patterns like "a*", "a*b*", which can match empty string
-        for (int j = 2; j <= n; j += 2)
+        while (left < right)
         {
-            if (p[j - 1] == '*')
+            int currentHeight = min(height[left], height[right]);
+            maxArea = max(maxArea, (currentHeight * (right - left)));
+
+            if (currentHeight == height[left])
             {
-                dp[0][j] = dp[0][j - 2];
+                left++;
+            }
+            else
+            {
+                right--;
             }
         }
-
-        // Filling the DP table
-        for (int i = 1; i <= m; i++)
-        {
-            for (int j = 1; j <= n; j++)
-            {
-                if (p[j - 1] == s[i - 1] || p[j - 1] == '.')
-                {
-                    dp[i][j] = dp[i - 1][j - 1]; // Match previous character
-                }
-                else if (p[j - 1] == '*')
-                {
-                    dp[i][j] = dp[i][j - 2]; // Ignore previous character
-
-                    if (p[j - 2] == s[i - 1] || p[j - 2] == '.')
-                    {
-                        dp[i][j] = dp[i][j] || dp[i - 1][j]; // Use previous character
-                    }
-                }
-            }
-        }
-
-        return dp[m][n];
+        return maxArea;
     }
 };
-
-int index()
-{
-    Solution solution;
-    string s = "mississippi";
-    string p = "mis*is*p*.";
-
-    cout << (solution.isMatch(s, p) ? "True" : "False") << endl;
-    return 0;
-}
