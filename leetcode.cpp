c51a9eb5d461d5f2b61eee9dd7cc7c144230b368
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2025-02-05 18:42:14 +0500

    Letter combination in digits.

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,38 +1,35 @@
 class Solution
 {
 public:
-    int threeSumClosest(vector<int> &nums, int target)
+    vector<string> letterCombinations(string digits)
     {
-        sort(nums.begin(), nums.end());
-        int closestSum = nums[0] + nums[1] + nums[2];
+        if (digits.empty())
+            return {}; // Edge case: empty input
 
-        for (int i = 0; i < nums.size() - 2; i++)
+        unordered_map<char, string> phoneMap = {
+            {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
+
+        vector<string> result;
+        string current;
+        backtrack(digits, 0, current, result, phoneMap);
+        return result;
+    }
+
+private:
+    void backtrack(const string &digits, int index, string &current, vector<string> &result, unordered_map<char, string> &phoneMap)
+    {
+        if (index == digits.size())
         {
-            int left = i + 1, right = nums.size() - 1;
-
-            while (left < right)
-            {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (abs(target - sum) < abs(target - closestSum))
-                {
-                    closestSum = sum;
-                }
-
-                if (sum < target)
-                {
-                    left++;
-                }
-                else if (sum > target)
-                {
-                    right--;
-                }
-                else
-                {
-                    return sum;
-                }
-            }
+            result.push_back(current); // Found a valid combination
+            return;
         }
-        return closestSum;
+
+        string letters = phoneMap[digits[index]];
+        for (char letter : letters)
+        {
+            current.push_back(letter);                               // Choose a letter
+            backtrack(digits, index + 1, current, result, phoneMap); // Explore further
+            current.pop_back();                                      // Backtrack (remove last choice)
+        }
     }
 };
