d85d22cacf3d579677419fd02eba28d6ad7ecd04
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2025-02-05 07:44:29 +0500

    Closest 3 sum in C ++

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,45 +1,38 @@
 class Solution
 {
 public:
-    vector<vector<int>> threeSum(vector<int> &nums)
+    int threeSumClosest(vector<int> &nums, int target)
     {
-        vector<vector<int>> res;
         sort(nums.begin(), nums.end());
+        int closestSum = nums[0] + nums[1] + nums[2];
 
-        for (int i = 0; i < nums.size(); i++)
+        for (int i = 0; i < nums.size() - 2; i++)
         {
-            if (i > 0 && nums[i] == nums[i - 1])
+            int left = i + 1, right = nums.size() - 1;
+
+            while (left < right)
             {
-                continue;
-            }
+                int sum = nums[i] + nums[left] + nums[right];
 
-            int j = i + 1;
-            int k = nums.size() - 1;
-
-            while (j < k)
-            {
-                int total = nums[i] + nums[j] + nums[k];
-
-                if (total > 0)
+                if (abs(target - sum) < abs(target - closestSum))
                 {
-                    k--;
+                    closestSum = sum;
                 }
-                else if (total < 0)
+
+                if (sum < target)
                 {
-                    j++;
+                    left++;
+                }
+                else if (sum > target)
+                {
+                    right--;
                 }
                 else
                 {
-                    res.push_back({nums[i], nums[j], nums[k]});
-                    j++;
-
-                    while (nums[j] == nums[j - 1] && j < k)
-                    {
-                        j++;
-                    }
+                    return sum;
                 }
             }
         }
-        return res;
+        return closestSum;
     }
 };
