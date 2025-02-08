43eb9c5f6d8511228b31cc8a5b82411c4a46cfd8
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2025-02-08 22:54:44 +0500

    Merging two arrays in TS

---
+++ leetcode.ts
@@ -0,0 +1,25 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val ?? 0;
+    this.next = next ?? null;
+  }
+}
+
+function mergeTwoLists(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+}
