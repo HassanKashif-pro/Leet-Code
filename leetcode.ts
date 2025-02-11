0c846fb766e60ae7e377e28de36bf9bd17e59140
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2025-02-11 19:58:16 +0500

    Max Depth problem in TS

--- leetcode.ts
+++ leetcode.ts
@@ -1,3 +1,17 @@
-function isAnagram(s: string, t: string): boolean {
-  return s.split("").sort().join("") === t.split("").sort().join("");
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val ?? 0;
+    this.left = left ?? null;
+    this.right = right ?? null;
+  }
+}
+
+function maxDepth(root: TreeNode | null): number {
+  if (!root) return 0;
+
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 }
