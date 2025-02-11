02a8dcf22cae70ecac2dc75c32d5ac57b8a46402
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2025-02-11 17:01:16 +0500

    Checking anagram in TS

--- leetcode.ts
+++ leetcode.ts
@@ -1,18 +1,3 @@
-class TreeNode {
-  val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
-}
-
-function invertTree(root: TreeNode | null): TreeNode | null {
-  if (!root) return null;
-
-  let temp = root.left;
-  root.left = root.right;
-  root.right = temp;
-
-  invertTree(root.right);
-  invertTree(root.left);
-
-  return root;
+function isAnagram(s: string, t: string): boolean {
+  return s.split("").sort().join("") === t.split("").sort().join("");
 }
