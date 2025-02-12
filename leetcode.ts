e748a2bc1f813254d76d71d768d09c010600020f
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2025-02-12 14:38:54 +0500

    Checking for single numbers in TS

--- leetcode.ts
+++ leetcode.ts
@@ -1,25 +1,9 @@
-class ListNode {
-  val: number;
-  next: ListNode | null;
+function singleNumber(nums: number[]): number {
+  let result = 0;
 
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = val ?? 0;
-    this.next = next ?? null;
-  }
-}
-
-function hasCycle(head: ListNode | null): boolean {
-  let slow = head;
-  let fast = head;
-
-  while (fast !== null && fast.next !== null) {
-    slow = slow!.next;
-    fast = fast.next.next;
-
-    if (slow === fast) {
-      return true;
-    }
+  for (let num of nums) {
+    result ^= num;
   }
 
-  return false;
+  return result;
 }
