09308c36d21784b9ac8e1e572f7d5a74da46cfc0
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2025-02-14 19:35:12 +0500

    Diameter of a binary tree problem.

--- leetcode.ts
+++ leetcode.ts
@@ -1,13 +1,28 @@
-/**
- Do not return anything, modify s in-place instead.
- */
-function reverseString(s: string[]): void {
-  let left = 0;
-  let right = s.length - 1;
-
-  while (left < right) {
-    [s[left], s[right]] = [s[right], s[left]];
-    left++;
-    right--;
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
   }
+}
+
+function diameterOfBinaryTree(root: TreeNode | null): number {
+  let maxDepth = 0;
+
+  function depth(node: TreeNode | null): number {
+    if (!node) return 0;
+
+    let leftDepth = depth(node.left);
+    let rightDepth = depth(node.right);
+
+    maxDepth = Math.max(maxDepth, leftDepth + rightDepth);
+
+    return Math.max(leftDepth, rightDepth) + 1;
+  }
+
+  depth(root);
+  return maxDepth;
 }
